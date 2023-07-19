ecd6820c08d10138b0da10f990680fbf85da6d1b
Joao Pedro Igeski Morais <joaopedroigeski@gmail.com>
2023-07-19 10:38:56 -0300

    API creation

---
+++ src/main/java/com/example/swplanetapi/domain/PlanetRepository.java
@@ -0,0 +1,7 @@
+package com.example.swplanetapi.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanetRepository extends JpaRepository<Planet, Long>{
+
+}
