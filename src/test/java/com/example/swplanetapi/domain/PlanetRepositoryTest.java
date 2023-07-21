cfd48d6ad188b08d738ae605bb24753581e658e5
Joao Pedro Igeski Morais <joaopedroigeski@gmail.com>
2023-07-21 09:49:07 -0300

    testing invalid data and existing planet creation - repository

--- src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
+++ src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
@@ -5,6 +5,7 @@ import static com.example.swplanetapi.common.PlanetConstants.INVALID_PLANET;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
@@ -25,5 +26,23 @@ public class PlanetRepositoryTest {
     assertThat(sut.getName()).isEqualTo(PLANET.getName());
     assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
     assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+  }
+
+  @Test
+  public void createPlanetWithInvalidData_ThrowsException(){
+    Planet emptyPlanet = new Planet();
+    Planet invalidPlanet = new Planet("", "", "");
+
+    assertThatThrownBy(() -> planetRepository.save(emptyPlanet)).isInstanceOf(RuntimeException.class);
+    assertThatThrownBy(() -> planetRepository.save(invalidPlanet)).isInstanceOf(RuntimeException.class);
+  }
+
+  @Test
+  public void createPlanet_WithExistingName_ThrowsException() {
+    Planet planet = testEntityManager.persistFlushFind(PLANET); // Salvar, atualizar no banco e recuperar
+    testEntityManager.detach(planet); // O entity manager deixa de monitorar o planeta
+    planet.setId(null); // Zera o ID, quando tentar salvar o JPA vai saber que é pra salvar
+    
+    assertThatThrownBy(() ->planetRepository.save(planet)).isInstanceOf(RuntimeException.class);;
   }
 }
